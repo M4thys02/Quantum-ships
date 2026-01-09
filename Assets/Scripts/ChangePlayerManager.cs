58d600f11a2bc1651dad93c877f12abdd2b8a6c3
M4thys02 <matej.marousek@seznam.cz>
2026-01-09 12:13:03 +0100

    Added tile counter text
    New indicator on each tile witch attack or guessed squares. It says how many squares are on the specific tile.

--- Assets/Scripts/ChangePlayerManager.cs
+++ Assets/Scripts/ChangePlayerManager.cs
@@ -21,16 +21,24 @@ public class ChangePlayerManager : MonoBehaviour {
     private int currentPlayer = 0;
     public int CurrentPlayer => currentPlayer;
 
-    // FOR PLAYER ATTACK:
+    //PLAYER ATTACK & GUESS:
+    [Header("Player attacking and guessing")]
+    [SerializeField] private Camera mainCamera;
     [SerializeField] private GameObject attackSquarePrefab;
     [SerializeField] private GameObject guessedSquarePrefab;
-    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Transform attackSqrtRoot;
+    [SerializeField] private Transform guessedSqrtRoot;
     [SerializeField] private int defaultSquares = 10;
     [SerializeField] private int maxAttackSquares;
     [SerializeField] private int currentAttackSquares;
+    [SerializeField] private TextMeshPro tileCounterPrefab;
+    [SerializeField] private Transform countersRoot;
 
     private Dictionary<Vector3Int, List<GameObject>> player0Squares = new Dictionary<Vector3Int, List<GameObject>>();
     private Dictionary<Vector3Int, List<GameObject>> player1Squares = new Dictionary<Vector3Int, List<GameObject>>();
+
+    private Dictionary<Vector3Int, TMP_Text> player0Counters = new();
+    private Dictionary<Vector3Int, TMP_Text> player1Counters = new();
 
     private void Awake() {
         gridSize = (int)PlayerPrefs.GetFloat("GridSlider", defaultGridSize);
@@ -81,10 +89,11 @@ public class ChangePlayerManager : MonoBehaviour {
                 return;
 
             Vector3 spawnPos = tilemap.GetCellCenterWorld(cellPos);
-            GameObject square = Instantiate(attackSquarePrefab, spawnPos, Quaternion.identity);
+            GameObject square = Instantiate(attackSquarePrefab, spawnPos, Quaternion.identity, attackSqrtRoot);
             square.transform.localScale = Vector3.one * gridScale;
             list.Add(square);
             currentAttackSquares++;
+            UpdateTileCounter(cellPos, list.Count, tilemap, GetActiveCountersDict());
         }
         else {
             if (list.Count == 0)
@@ -94,7 +103,30 @@ public class ChangePlayerManager : MonoBehaviour {
             list.RemoveAt(list.Count - 1);
             Destroy(square);
             currentAttackSquares--;
+            UpdateTileCounter(cellPos, list.Count, tilemap, GetActiveCountersDict());
         }
+    }
+
+    private void UpdateTileCounter(Vector3Int cellPos, int count, Tilemap tilemap,  Dictionary<Vector3Int, TMP_Text> countersDict) {
+        if (count <= 0) {
+            if (countersDict.TryGetValue(cellPos, out var oldText)) {
+                Destroy(oldText.gameObject);
+                countersDict.Remove(cellPos);
+            }
+            return;
+        }
+
+        if (!countersDict.TryGetValue(cellPos, out var text)) {
+            Vector3 pos = tilemap.GetCellCenterWorld(cellPos);
+
+            text = Instantiate(tileCounterPrefab, countersRoot);
+            text.transform.position = pos;
+            //text.transform.localScale = Vector3.one * gridScale;
+
+            countersDict[cellPos] = text;
+        }
+
+        text.text = count.ToString();
     }
 
     private void GenerateTilemap(Tilemap currentTilemap) {
@@ -126,6 +158,9 @@ public class ChangePlayerManager : MonoBehaviour {
 
         SetSquaresVisibility(player0Squares, isPlayer0);
         SetSquaresVisibility(player1Squares, !isPlayer0);
+
+        SetCountersVisibility(player0Counters, isPlayer0);
+        SetCountersVisibility(player1Counters, !isPlayer0);
     }
 
     private void SetSquaresVisibility(Dictionary<Vector3Int, List<GameObject>> dict, bool visible) {
@@ -134,6 +169,13 @@ public class ChangePlayerManager : MonoBehaviour {
                 if (square != null)
                     square.SetActive(visible);
             }
+        }
+    }
+
+    private void SetCountersVisibility(Dictionary<Vector3Int, TMP_Text> dict, bool visible) {
+        foreach (var kv in dict) {
+            if (kv.Value != null)
+                kv.Value.gameObject.SetActive(visible);
         }
     }
 
@@ -151,6 +193,9 @@ public class ChangePlayerManager : MonoBehaviour {
 
     public int GetOpponent() {
         return currentPlayer ^ 1;
+    }
+    private Dictionary<Vector3Int, TMP_Text> GetActiveCountersDict() {
+        return currentPlayer == 0 ? player0Counters : player1Counters;
     }
     public void CreateGuessedSquares(Vector3Int tilePos3D, int amount) { //TODO: not only 10 squares in one tile, but 10 squares only in GLOBAL
         Dictionary<Vector3Int, List<GameObject>> playerSquares = GetActiveSquaresDict();
@@ -171,10 +216,12 @@ public class ChangePlayerManager : MonoBehaviour {
 
         // create guessed squares INSTEAD of attack squares
         for (int i = 0; i < amount; i++) {
-            GameObject guessed = Instantiate(guessedSquarePrefab, spawnPos, Quaternion.identity);
+            GameObject guessed = Instantiate(guessedSquarePrefab, spawnPos, Quaternion.identity, guessedSqrtRoot);
             guessed.transform.localScale = Vector3.one * gridScale;
             list.Add(guessed);
         }
+
+        UpdateTileCounter(tilePos3D, amount, GetActiveTilemap(), GetActiveCountersDict());
     }
 
     bool IsTileAlreadyGuessed(Dictionary<Vector3Int, List<GameObject>> squares, Vector3Int tile) {
