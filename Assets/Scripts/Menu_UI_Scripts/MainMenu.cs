0caf9590dc952e7dbec2a02518c25807ea854284
M4thys02 <matej.marousek@seznam.cz>
2026-01-23 22:40:51 +0100

    Fixed #12

--- Assets/Scripts/Menu_UI_Scripts/MainMenu.cs
+++ Assets/Scripts/Menu_UI_Scripts/MainMenu.cs
@@ -3,16 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
     public LevelLoader _levelLoader;
+    [SerializeField] private GameObject _blockPanel;
     public void GoToScene(string sceneName) {
         SceneManager.LoadScene(sceneName);
     }
 
     public void StartGame(string sceneName) {
+        _blockPanel.SetActive(true);
         _levelLoader.loadGame(sceneName);
     }
 
     public void quitApp() {
         Application.Quit();
-        //Debug.Log("Application has quit");
     }
 }
